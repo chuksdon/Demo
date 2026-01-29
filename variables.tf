8b547d8459d427dff18f7c95fcbff9cf3a0e2833
chuksdon <chuksuyammadu@gmail.com>
2026-01-28 18:32:58 -0600

    New

---
+++ variables.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region  = var.region
+  profile = "my-profile"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+terraform {
+  required_version = ">= 1.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
